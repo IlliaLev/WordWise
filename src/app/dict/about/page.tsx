98b75d6a826fcd2586ec643062547a9d0c986641
IlliaLev <illialevuoi@gmail.com>
2025-11-22 18:31:02 +0200

    cleared about page

--- src/app/dict/about/page.tsx
+++ src/app/dict/about/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import Checkbox from "@/components/ui/Checkbox";
-import { useState } from "react";
-
 export default function AboutPage() {
-    const [checked, setChecked] = useState(false);
 
     return (
-        <div>
-            <Checkbox checked={checked} onChange={setChecked} >
-
-            </Checkbox>
-        </div>
+        <div></div>
     );
 }
