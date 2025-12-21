dc4e6997f4c9797b9714baaceccdd1d12390ae27
IlliaLev <illialevuoi@gmail.com>
2025-12-21 21:16:28 +0200

    added logo for tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 
 import "./globals.css";
 import ScrollToTopButton from "@/components/ui/ScrollToTopButton";
